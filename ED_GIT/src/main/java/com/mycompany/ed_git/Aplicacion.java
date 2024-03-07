81dced54d5f04a978fed02ebc44b35702cbbd07e
sandrix23 <159067757+sandrix23@users.noreply.github.com>
2024-03-07 11:54:37 +0100

    calcular
    calcular la hipotenusa y el area

---
+++ ED_GIT/src/main/java/com/mycompany/ed_git/Aplicacion.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.ed_git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Aplicacion {
+
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo de programa para git");
+        int opcion = 0;
+        while (opcion != 4) {
+            System.out.println("¿Que quieres hacer?");
+            System.out.println(" 1.Calcular la hipotenusa de un triangulo\n 2.Calcular el área de un cilindro\n 3.Salir");
+            opcion = entrada.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    hipotenusa();
+                    break;
+
+                case 2:
+                    area();
+                    break;
+
+                case 3:
+                    System.out.println("Gracias por usar el programa");
+                    break;
+
+            }
+        }
+    }
+
+    public static void hipotenusa() {
+        System.out.println("Introduce un cateto");
+        int cateto1 = entrada.nextInt();
+        System.out.println("Introduce el otro ");
+        int cateto2 = entrada.nextInt();
+        double resultadohipotenusa = Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
+        System.out.println("La hipotenusa es: " + resultadohipotenusa);
+    }
+
+    public static void area() {
+        System.out.println("Escribe el radio");
+        int r = entrada.nextInt();
+        System.out.println("Escribe la altura");
+        int h = entrada.nextInt();
+        double resultadoarea = (2 * Math.PI * r * (r + h));
+        System.out.println("El area es: " + resultadoarea);
+    }
+
+}
